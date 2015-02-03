c561937da78f9078bad6b93b962885db6ba74beb
Kate Beavis <katebeavis84@gmail.com>
2015-02-03 16:03:25 +0000

    Added docking_station_spec and docking_station

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,27 @@
+require 'docking_station'
+
+describe DockingStation  do
+
+	let(:bike) { Bike.new }
+	let(:station) { DockingStation.new(capacity: 20) }
+
+	it 'should accept a bike' do
+		bike = Bike.new
+		station = DockingStation.new
+		expect(station.bike_count).to eq(0)
+		station.dock(bike)
+		expect(station.bike_count).to eq(1)
+	end
+
+	it 'should release a bike' do
+		station.dock(bike)
+		station.release(bike)
+		expect(station.bike_count).to eq(0)
+	end
+
+	it 'should know when it\'s full' do
+		expect(station).not_to be_full
+		20.times { station.dock(Bike.new) }
+		expect(station).to be_full
+	end
+end
