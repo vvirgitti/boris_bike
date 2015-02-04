271aaab5990df16b2c0715161ad8df21ff38e537
Kate Beavis <katebeavis84@gmail.com>
2015-02-04 09:19:41 +0000

    Added bike_container and refactored docking_station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,31 +1,10 @@
+require_relative 'bike_container'
+
 class DockingStation
-	DEFAULT_CAPACITY = 10
 
-def initialize(options = {})
-	@capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
-	@bikes = []
-end
+	include BikeContainer
 
-	def bike_count()
-		@bikes.count
+	def initialize(options = {})
+		self.capacity = options.fetch(:capacity, capacity)
 	end
-
-	def dock(bike)
-		raise 'Station is full' if full?
-		@bikes << bike
-	end
-
-	def release(bike)
-		@bikes.delete(bike)
-	end
-
-	def full?
-		bike_count == @capacity
-	end
-
-	def available_bikes
-		@bikes.reject {|bike| bike.broken? }
-	end
-	
-
 end
