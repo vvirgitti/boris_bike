735e1b2ed1dabe7a1472f62b9c7fe7e2eaea5ab6
Vanessa <virgitti@gmail.com>
2015-02-05 12:30:10 +0000

    New test for van

--- lib/van.rb
+++ lib/van.rb
@@ -10,6 +10,6 @@ class Van
 		end
 	end
 
-
-
+	
+	
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -5,19 +5,14 @@ require 'docking_station'
 describe Van do
 
 	let(:van) { Van.new }
+	let(:broken_bike) {double :bike, broken?: true}
 	
-
-	it 'should only accept broken bikes when it goes to the garage' do
-		bike.break!
-		expect(bike).to be_broken
-	end
 
 	it 'can collect broken bikes from a docking station' do
 		# we need a station with broken bikes
 		# we need a van, broken bike
 		# when i collect the broken bikes from station, i have those bikes and the station doesn't
 
-		broken_bike = double :bike, broken?: true
 		station     = double :station, broken_bikes: [broken_bike]
 
 		expect(station).to receive(:release).with(broken_bike).and_return(broken_bike)
@@ -25,8 +20,19 @@ describe Van do
 		van.collect_broken_bikes_from(station)
 
 		expect(van.unavailable_bikes).to eq [broken_bike]
-
 	end
 
+
+	it 'can deliver broken bikes to the garage' do 
+		# we need a van with broken bikes
+		# we need a garage, broken bikes
+		# when I deliver the broken bikes to the garage, the garage have them and I don't
+
+		van = double :van, broken_bikes: [broken_bike]
+
+		expect(van).to receive(:release).with(broken_bike).and_return(broken_bike)
+
+	end
+	
 
 end
